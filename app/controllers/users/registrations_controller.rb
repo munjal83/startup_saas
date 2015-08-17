9760f9ca76572f478faa04845d0b312668f7c770
munjal83 <munjalbpatel@hotmail.com>
2015-08-17 11:13:19 +0000

    modified the devise registrations files

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,14 @@
+Class Users::RegistrationsController < Devise::RegistrationsController
+
+def create
+    super do |resource|
+        if params[:plan]
+            resource.plan_id = 2
+            resource.save_with_payment
+        else
+            resource.save   
+        end
+    end
+end
+end
+end
