dd2941db05e1cd37740f5602f75c826da3ee4fab
munjal83 <munjalbpatel@hotmail.com>
2015-08-19 07:01:09 +0000

    added new action for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
-class PagesController < ApplicationController
-
+class ProfilesController < ApplicationController
+    
+    def new
+        #form for each user to fill out their own profile
+        @user = User.find(params[:user_id])
+        @profile = @user.build_profile
+    end
     
 end
